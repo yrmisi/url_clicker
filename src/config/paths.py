acff6708e56b2b1fe336ddeeb7b9b1bd24452e0d
yrmisi <yrmisi@example.com>
2026-03-02 00:57:15 +0300

    add paths

--- src/config/paths.py
+++ src/config/paths.py
@@ -5,3 +5,5 @@ BASE_DIR: Path = Path(__file__).resolve().parent.parent
 ENVS_DIR: Path = BASE_DIR / "config" / "envs"
 
 TEMPLATES_DIR: Path = BASE_DIR / "templates"
+
+LOCALES_DIR: Path = BASE_DIR / "locales"
