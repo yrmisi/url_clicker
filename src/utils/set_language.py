a7c3301b81f18cd80b41e15161c49deb5fe43f09
yrmisi <yrmisi@example.com>
2026-03-02 01:00:06 +0300

    create preferred language

---
+++ src/utils/set_language.py
@@ -0,0 +1,7 @@
+from fastapi import Request
+
+
+def get_preferred_language(request: Request) -> str:
+    """Determines the user's language."""
+    accept_lang: str = request.headers.get("accept-language", "en").lower()
+    return "ru" if accept_lang.startswith("ru") else "en"
