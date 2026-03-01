b359bdfc619bb5e31341b697ceb082523f11b755
yrmisi <yrmisi@example.com>
2026-03-02 01:03:15 +0300

    create load tranclations to cache

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,5 +1,8 @@
+from .i18n import load_translations_to_cache, translations_cache
 from .redis_client import get_redis
 
 __all__ = [
     "get_redis",
+    "load_translations_to_cache",
+    "translations_cache",
 ]
