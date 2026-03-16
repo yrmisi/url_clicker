0cb008de1f5963556e76c858a4fdb09cb0a9ee6e
yrmisi <yrmisi@example.com>
2026-03-16 22:31:02 +0300

    add docsring in exceptions/slug_already_exists_db.py file

--- src/exceptions/slug_already_exists_db.py
+++ src/exceptions/slug_already_exists_db.py
@@ -4,5 +4,7 @@ from .shortener_base import ShortenerBaseError
 
 
 class SlugAlreadyExistsDBError(ShortenerBaseError):
+    """Raised when a unique slug cannot be generated after multiple attempts due to existing records in the database."""
+
     status_code: int = status.HTTP_500_INTERNAL_SERVER_ERROR
     detail: str = "Failed to generate unique slug after 5 attempts"
