8d88d9adf9327c14538ead92169065810c50e9d4
yrmisi <yrmisi@example.com>
2026-03-16 22:39:17 +0300

    add docsring in exceptions/invalid_url.py file

--- src/exceptions/invalid_url.py
+++ src/exceptions/invalid_url.py
@@ -4,5 +4,7 @@ from .shortener_base import ShortenerBaseError
 
 
 class InvalidURLError(ShortenerBaseError):
+    """Raised when the provided URL fails validation."""
+
     status_code: int = status.HTTP_422_UNPROCESSABLE_CONTENT
     detail: str = "invalid URL"
