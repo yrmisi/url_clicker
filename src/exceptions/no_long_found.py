3e734cbf5787b8602ba7de764ba5592f039f1aa1
yrmisi <yrmisi@example.com>
2026-03-16 22:32:29 +0300

    add docsring in exceptions/no_long_found.py file

--- src/exceptions/no_long_found.py
+++ src/exceptions/no_long_found.py
@@ -4,5 +4,7 @@ from .shortener_base import ShortenerBaseError
 
 
 class NoLongFoundError(ShortenerBaseError):
+    """Raised when a long URL for the given slug cannot be found."""
+
     status_code: int = status.HTTP_404_NOT_FOUND
     detail: str = "The link does not exist"
