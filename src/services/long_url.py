8d34075fc708137fa8f70787d6c92d9e937c7053
yrmisi <yrmisi@example.com>
2026-03-16 22:24:21 +0300

    add docsring in long_url.py file

--- src/services/long_url.py
+++ src/services/long_url.py
@@ -4,7 +4,7 @@ from exceptions import NoLongFoundError
 
 
 async def get_long_url(slug: str, session: AsyncSessionDep) -> str:
-    """ """
+    """Return the original long URL for the given slug or raise NoLongFoundError if not found."""
     long_url: str | None = await get_long_url_by_slug_db(slug, session)
     if long_url is None:
         raise NoLongFoundError()
