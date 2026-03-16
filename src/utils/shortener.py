30a8684220d6c0090df7e406a089747df2f09b13
yrmisi <yrmisi@example.com>
2026-03-16 22:05:08 +0300

    add docsring in shortener.py file

--- src/utils/shortener.py
+++ src/utils/shortener.py
@@ -5,5 +5,5 @@ ALNUM: str = string.ascii_letters + string.digits
 
 
 def generate_slug(length: int = 6):
-    """ """
+    """Generate a random alphanumeric slug of the given length."""
     return "".join(secrets.choice(ALNUM) for _ in range(length))
