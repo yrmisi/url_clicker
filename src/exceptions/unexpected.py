c7d13f0999f344fb16093a74cc4cf31f2cf1a2f6
yrmisi <yrmisi@example.com>
2026-03-16 22:29:25 +0300

    add docsring in exceptions/unexpected.py file

--- src/exceptions/unexpected.py
+++ src/exceptions/unexpected.py
@@ -4,5 +4,7 @@ from .shortener_base import ShortenerBaseError
 
 
 class UnexpectedError(ShortenerBaseError):
+    """Raised when an unexpected error occurs after multiple attempts."""
+
     status_code: int = status.HTTP_500_INTERNAL_SERVER_ERROR
     detail: str = "Unexpected error after 5 attempts"
